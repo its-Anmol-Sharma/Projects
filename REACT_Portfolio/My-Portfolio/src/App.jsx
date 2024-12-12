38379be0381fd56670aef1b85866dbd7e1ab29cc
its-Anmol-Sharma <144224825+its-Anmol-Sharma@users.noreply.github.com>
2024-12-13 02:36:07 +0530

    My React Portfolio

---
+++ REACT_Portfolio/My-Portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+
+import style from './App.module.css'
+import { Navbar } from './components/Navbar/Navbar'
+
+function App() {
+ 
+
+  return (
+    <div className={style.App}>
+     <Navbar/>
+    </div>
+  )
+}
+
+export default App
