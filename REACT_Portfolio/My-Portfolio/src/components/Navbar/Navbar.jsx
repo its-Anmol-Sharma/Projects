ce8f3bef6201192ddaadca05958ad186a1735072
its-Anmol-Sharma <144224825+its-Anmol-Sharma@users.noreply.github.com>
2024-12-14 02:46:32 +0530

    Created a navbar

--- REACT_Portfolio/My-Portfolio/src/components/Navbar/Navbar.jsx
+++ REACT_Portfolio/My-Portfolio/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
+import open from "../../../assets/nav/menuIcon.png";
+import close from "../../../assets/nav/closeIcon.png";
+import styles from './Navbar.module.css';
 
 export const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [ menuOpen, setMenuOpen] = useState(false);
+  // useState - Because we need to keep track of wheater the Hamburger Menu is Open or not 
+  
+
+  return(
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/"> Portfolio </a>
+        <div className={styles.menu}>
+          <img className={styles.menuBtn}
+          src={menuOpen
+            ? close   
+            : open }
+            alt="menu-button"
+            onClick={() => setMenuOpen(!menuOpen)} />
+          <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}>
+            <li><a href="#about">About</a></li>
+            <li><a href="#experience">Experience</a></li>        
+            <li><a href="#projects">Project</a></li>
+            <li><a href="#contact">Contact</a></li>
+        
+          </ul>
+      </div>
+    </nav>
+  );
+};
